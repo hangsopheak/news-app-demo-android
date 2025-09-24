631e3d36bd54a0acb15bcbb90cb693944b1eabf5
sopheak <hangsopheak.hod@gmail.com>
2025-09-24 16:19:06 +0700

    Onboarding feature

--- app/src/main/java/com/rupp/news_app_demo_android/MainActivity.kt
+++ app/src/main/java/com/rupp/news_app_demo_android/MainActivity.kt
@@ -5,25 +5,45 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.rupp.news_app_demo_android.ui.onboarding.OnboardingScreen
 import com.rupp.news_app_demo_android.ui.theme.NewsappdemoandroidTheme
+import com.rupp.news_app_demo_android.utils.OnboardingUtil
 
 class MainActivity : ComponentActivity() {
+    private val onboardingUtil by lazy { OnboardingUtil(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NewsappdemoandroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            var isOnboardingCompleted by remember {
+                mutableStateOf(onboardingUtil.isOnboardingCompleted())
+            }
+
+            NewsappdemoandroidTheme(dynamicColor = false) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    if (isOnboardingCompleted) {
+                        Greeting("Sopheak")
+                    } else {
+                        OnboardingScreen(
+                            onFinished = {
+                                onboardingUtil.setOnboardingCompleted()
+                                isOnboardingCompleted = true
+                            }
+                        )
+                    }
                 }
             }
         }
