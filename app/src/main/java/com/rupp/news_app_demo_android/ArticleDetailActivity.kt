8df5b4c7b59cf6c1e609bfff3d6aaccb0902fe76
sopheak <hangsopheak.hod@gmail.com>
2025-09-28 21:29:13 +0700

    jetpack compose extend - explore screen and detail screen

---
+++ app/src/main/java/com/rupp/news_app_demo_android/ArticleDetailActivity.kt
@@ -0,0 +1,50 @@
+package com.rupp.news_app_demo_android
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.rupp.news_app_demo_android.data.ArticleData
+import com.rupp.news_app_demo_android.model.Article
+import com.rupp.news_app_demo_android.ui.article.ArticleScreen
+import com.rupp.news_app_demo_android.ui.theme.NewsappdemoandroidTheme
+
+class ArticleDetailActivity : ComponentActivity() {
+
+    private var articleId:Int = 0;
+    override fun onStart() {
+        super.onStart()
+        articleId = intent.getIntExtra("article_id", 0)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NewsappdemoandroidTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val article: Article? = ArticleData.allArticles.find { it.id == articleId }
+                    if (article != null) {
+                        ArticleScreen(article = article, onBackClick = { finish() })
+                    } else {
+                        NoArticleFound()
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun NoArticleFound() {
+        Text("No article")
+    }
+}
---
+++ app/src/main/java/com/rupp/news_app_demo_android/ui/explore/presentation/HeroArticleCard.kt
@@ -0,0 +1,53 @@
+package com.rupp.news_app_demo_android.ui.explore.presentation
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.rupp.news_app_demo_android.data.ArticleData
+import com.rupp.news_app_demo_android.model.Article
+import com.rupp.news_app_demo_android.ui.theme.NewsappdemoandroidTheme
+
+@Composable
+fun HeroArticleCard(modifier: Modifier = Modifier, article: Article, onClick: () -> Unit = {}) {
+    Column(
+        modifier = modifier
+            .padding(5.dp)
+            .fillMaxWidth().clickable(onClick = onClick)
+    ) {
+        AsyncImage(
+            model = article.imageUrl,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .clip(RoundedCornerShape(8.dp)),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        ArticleTitleSection(article = article)
+        Spacer(modifier = Modifier.size(20.dp))
+    }
+}
+
+
+
+@Composable
+@Preview(showBackground = true)
+fun HeroArticleCardPreview() {
+    NewsappdemoandroidTheme {
+        HeroArticleCard(Modifier, ArticleData.allArticles[3])
+    }
+
+}
