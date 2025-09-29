4d1282e0d2ea8e92ead6d516d43b5ba644f2b242
sopheak <hangsopheak.hod@gmail.com>
2025-09-29 22:01:32 +0700

    network util
    (cherry picked from commit b034d392fe2585ff4ee8e403e7b33eff08746927)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,9 @@ dependencies {
 
     implementation("io.coil-kt:coil-compose:2.2.2")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
