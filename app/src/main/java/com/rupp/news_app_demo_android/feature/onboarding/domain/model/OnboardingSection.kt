876cf6b7bd5520d880fc17c6ae0d018bf99fee4f
sopheak <hangsopheak.hod@gmail.com>
2025-09-29 21:51:22 +0700

    app architecture - refactor to modular packages

---
+++ app/src/main/java/com/rupp/news_app_demo_android/feature/onboarding/domain/model/OnboardingSection.kt
@@ -0,0 +1,8 @@
+package com.rupp.news_app_demo_android.feature.onboarding.domain.model
+
+data class OnboardingSection(
+    val title: String,
+    val subtitle: String,
+    val description: String,
+    val imageRes: Int
+)
