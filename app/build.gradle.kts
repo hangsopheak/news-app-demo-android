acf13941ecb67dba728eaf09e9e3615ef5b7c024
sopheak <hangsopheak.hod@gmail.com>
2025-10-01 21:10:15 +0700

    jetpack compose demo - e4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ android {
 
 dependencies {
 
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+    implementation("io.coil-kt:coil-compose:2.2.2")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
