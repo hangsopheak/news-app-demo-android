76b076ee9ab3964b50851cd16052dbe4d4a7ef66
sopheak <hangsopheak.hod@gmail.com>
2025-10-10 21:37:09 +0700

    ExploreScreen MVVM

---
+++ app/src/main/java/com/rupp/news_app_demo_android/feature/explore/presentation/ExploreUiState.kt
@@ -0,0 +1,22 @@
+package com.rupp.news_app_demo_android.feature.explore.presentation
+import com.rupp.news_app_demo_android.shared.domain.model.Article
+import com.rupp.news_app_demo_android.shared.domain.model.Category
+
+data class ExploreUiState(
+    val categoriesState: CategoriesUiState = CategoriesUiState(),
+    val articlesState: ArticlesUiState = ArticlesUiState()
+)
+
+
+data class CategoriesUiState(
+    val isLoading: Boolean = false,
+    val error: String? = null,
+    val categories: List<Category> = emptyList(),
+    val selectedCategoryId: Int? = null
+)
+
+data class ArticlesUiState(
+    val isLoading: Boolean = false,
+    val error: String? = null,
+    val articles: List<Article> = emptyList()
+)
