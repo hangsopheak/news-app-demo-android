a60cfa816c4b692e4373ae87dc5bc7a4715c8401
sopheak <hangsopheak.hod@gmail.com>
2025-10-19 16:48:23 +0700

    compose test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,6 +92,16 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.junit.ktx)
+    // Instrumented + Compose UI Test
+    androidTestImplementation("androidx.test:core:1.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Compose UI Test
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.7")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.7")
+
     // Hilt
     implementation("com.google.dagger:hilt-android:2.57.1")
     kapt("com.google.dagger:hilt-android-compiler:2.57.1")
@@ -123,4 +133,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    testImplementation(kotlin("test"))
 }
