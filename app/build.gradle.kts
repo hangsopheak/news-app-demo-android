e568245ebfdc60a0883e09900508e2458c3cfa2d
sopheak <hangsopheak.hod@gmail.com>
2025-10-29 09:30:58 +0700

    E4 build test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,11 +22,35 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // Expose gradle.properties into BuildConfig
+
+        buildConfigField("String", "API_BASE_URL", "\"${project.findProperty("API_BASE_URL")}\"")
+        buildConfigField("String", "DB_NAME", "\"${project.findProperty("DB_NAME")}\"")
     }
 
     buildTypes {
+        debug {
+            // Debug version points to dev/staging environment
+            buildConfigField("String", "API_BASE_URL", "\"${project.findProperty("API_BASE_URL")}\"")
+            buildConfigField("String", "DB_NAME", "\"${project.findProperty("DB_NAME")}\"")
+
+            // Helpful for debugging
+            isDebuggable = true
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
+
         release {
-            isMinifyEnabled = false
+            // Release version points to production
+            buildConfigField("String", "API_BASE_URL", "\"${project.findProperty("API_BASE_URL_PROD")}\"")
+            buildConfigField("String", "DB_NAME", "\"${project.findProperty("DB_NAME_PROD")}\"")
+
+            //isMinifyEnabled = true is a setting in your Android app's build.gradle.kts
+            // file that enables tools to make your app smaller and more secure for a release build. It activate
+            // 1. Code Shrinking (Minification)
+            // 2. Code Obfuscation
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -42,10 +66,45 @@ android {
     }
     buildFeatures {
         compose = true
+        android.buildFeatures.buildConfig = true
+    }
+
+    flavorDimensions += "edition"
+    productFlavors {
+        create("free") {
+            dimension = "edition"
+            applicationIdSuffix = ".free"
+            versionNameSuffix = "-free"
+
+            // Free edition config
+            buildConfigField("Boolean", "IS_PREMIUM", "false")
+        }
+
+        create("premium") {
+            dimension = "edition"
+            applicationIdSuffix = ".premium"
+            versionNameSuffix = "-premium"
+
+            // Premium edition config
+            buildConfigField("Boolean", "IS_PREMIUM", "true")
+        }
     }
 }
 
+
+
 dependencies {
+
+    implementation(libs.androidx.junit.ktx)
+    // Instrumented + Compose UI Test
+    androidTestImplementation("androidx.test:core:1.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Compose UI Test
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.7")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.7")
+
 
     // Hilt
     implementation("com.google.dagger:hilt-android:2.57.1")
